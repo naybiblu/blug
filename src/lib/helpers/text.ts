bf6bb7e14356ae734835bf2ee708464c323cda1e
mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph <mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph>
2023-04-08 15:14:20 +0800

    Fixed slugify function v1.5

--- src/lib/helpers/text.ts
+++ src/lib/helpers/text.ts
@@ -13,24 +13,8 @@ export const slugify = (str: string, url = false, urlify = true) => {
   if (!regex.test(str)) return str;
   
   for (var word of str.split(url ? "-" : " ")) {
-    switch (word) {
-      case "\\": strArr.push("[x+5c]"); break;
-      case "/": strArr.push("[x+2f]"); break;
-      case ":": strArr.push("[x+3a]"); break;
-      case "*": strArr.push("[x+2a]"); break;
-      case "?": strArr.push("[x+3f]"); break;
-      case "\"": strArr.push("[x+22]"); break;
-      case "<": strArr.push("[x+3c]"); break;
-      case ">": strArr.push("[x+3e]"); break;
-      case "|": strArr.push("[x+7c]"); break;
-      case "#": strArr.push("[x+23]"); break;
-      case "%": strArr.push("[x+25]"); break;
-      case "[": strArr.push("[x+5b]"); break;
-      case "]": strArr.push("[x+5d]"); break;
-      case "(": strArr.push("[x+28]"); break;
-      case ")": strArr.push("[x+29]"); break;
-      default: strArr.push(word);
-    }
+    if (!regex.test(word)) strArr.push(word);
+    else strArr.push("");
   }
   
   return urlify ? strArr.join(url ? "-" : " ").toLowerCase().replaceAll(" ", "-") : strArr.join(url ? "-" : " ");
